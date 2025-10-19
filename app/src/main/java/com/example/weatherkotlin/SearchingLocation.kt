327ec31c9a2f6b8fa828446148fb79682980e194
archuser-q <availableemailaddress123@tutamail.com>
2025-10-19 11:39:48 +0700

    updatee

--- app/src/main/java/com/example/weatherkotlin/SearchingLocation.kt
+++ app/src/main/java/com/example/weatherkotlin/SearchingLocation.kt
@@ -1,6 +1,8 @@
 package com.example.weatherkotlin
 
 import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.interaction.MutableInteractionSource
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
@@ -62,7 +64,7 @@ fun SearchingLocation(){
                 Text(
                     text = "Cancel",
                     fontWeight = FontWeight.Bold,
-                    fontSize = 16.sp)
+                    fontSize = 16.sp,)
             }
         }
         if (searchQuery.isNotBlank()) {
--- app/src/main/java/com/example/weatherkotlin/components/SearchingResult.kt
+++ app/src/main/java/com/example/weatherkotlin/components/SearchingResult.kt
@@ -1,5 +1,7 @@
 package com.example.weatherkotlin.components
 
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.interaction.MutableInteractionSource
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxWidth
@@ -7,6 +9,7 @@ import androidx.compose.foundation.layout.height
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
@@ -16,7 +19,12 @@ import com.example.weatherkotlin.LocationItem
 @Composable
 fun SearchingResult(location: LocationItem) {
     Column(
-        modifier = Modifier.fillMaxWidth()
+        modifier = Modifier
+            .fillMaxWidth()
+            .clickable(
+                indication = null,
+                interactionSource = remember { MutableInteractionSource() }
+            ) { }
     ) {
         Text(
             text = location.name,
